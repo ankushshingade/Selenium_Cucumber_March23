37735f10521b5ece83d4973b0e3f94bd2f34f6e5
ankushshingade <44308575+ankushshingade@users.noreply.github.com>
2023-03-24 18:16:05 +0530

    Updated "for" loop example.

--- src/Loops/For_Demo.java
+++ src/Loops/For_Demo.java
@@ -6,5 +6,9 @@ public class For_Demo {
         for(int i=1; i<=10; i++){
             System.out.println(i);
         }
+        // Using double as iteration variable.
+        for(double a=1; a<=10; a++){
+            System.out.println(a);
+        }
     }
 }
