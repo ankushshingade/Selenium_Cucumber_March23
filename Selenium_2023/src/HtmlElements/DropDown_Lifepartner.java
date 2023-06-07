71a6f0e307ec660f9895daf86f71dbcc5b0aa373
ankushshingade <44308575+ankushshingade@users.noreply.github.com>
2023-06-07 15:51:08 +0530

    Dropdown Homework update

--- Selenium_2023/src/HtmlElements/DropDown_Lifepartner.java
+++ Selenium_2023/src/HtmlElements/DropDown_Lifepartner.java
@@ -16,29 +16,29 @@ public class DropDown_Lifepartner {
 
         Thread.sleep(2000);
 
-        WebElement selTxt1 = driver.findElement(By.name("bg"));
-        Select sel1 = new Select(selTxt1);
+        WebElement LookingFor = driver.findElement(By.name("bg"));
+        Select sel1 = new Select(LookingFor);
         sel1.selectByVisibleText("Bride");
 
         Thread.sleep(2000);
 
-        WebElement selTxt2 = driver.findElement(By.name("a1"));
-        WebElement selTxt3 = driver.findElement(By.name("a2"));
-        Select sel2 = new Select(selTxt2);
-        Select sel3 = new Select(selTxt3);
+        WebElement startAge = driver.findElement(By.name("a1"));
+        WebElement endAge = driver.findElement(By.name("a2"));
+        Select sel2 = new Select(startAge);
+        Select sel3 = new Select(endAge);
         sel2.selectByVisibleText("25");
         sel3.selectByVisibleText("35");
 
         Thread.sleep(2000);
 
-        WebElement selTxt4 = driver.findElement(By.name("cm"));
-        Select sel4 = new Select(selTxt4);
+        WebElement community = driver.findElement(By.name("cm"));
+        Select sel4 = new Select(community);
         sel4.selectByVisibleText("Hindu:Marathi");
 
         Thread.sleep(2000);
 
-        WebElement selTxt5 = driver.findElement(By.name("ms"));
-        Select sel5 = new Select(selTxt5);
+        WebElement maritalStatus = driver.findElement(By.name("ms"));
+        Select sel5 = new Select(maritalStatus);
         sel5.selectByValue("1");
 
         Thread.sleep(5000);
